7730475699db6aeccbcb61d150798c945132c4ac
joao14 <barcealex1014@hotmail.com>
2022-06-10 13:06:10 -0500

    First structure

--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+var ()
 
 func main() {
-	fmt.Println("EJERCICIO")
+
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.ebay.com"),
+	)
+
+	c.UserAgent = "Solutions Exercices"
+
+	c.OnRequest(func(r *colly.Request) {
+
+		for key, value := range *r.Headers {
+			fmt.Printf("%s: %s\n", key, value)
+		}
+
+		fmt.Println(r.Method)
+	})
+
+	fmt.Println("ARTICULOS--")
+	c.OnHTML("li .s-item__wrapper", func(e *colly.HTMLElement) {
+		//product := Product{}
+		fmt.Println(e.ChildText(".s-item__title"))
+		fmt.Println(e.ChildText(".s-item__price"))
+		fmt.Println(e.ChildText(".SECONDARY_INFO"))
+		fmt.Println(e.ChildText(".s-item__link"))
+
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+
+		fmt.Println("-----------------------------")
+
+		fmt.Println(r.StatusCode)
+
+		for key, value := range *r.Headers {
+			fmt.Printf("%s: %s\n", key, value)
+		}
+	})
+
+	c.Visit("https://www.ebay.com/sch/i.html?_nkw=garlandcomputer")
+
 }
---
+++ model/Product.go
@@ -0,0 +1,8 @@
+package model
+
+type Product struct {
+	Title       string `json:"title"`
+	Price       string `json:"price"`
+	Product_url string `json:"product_url"`
+	Condition   string `json:"condition"`
+}
